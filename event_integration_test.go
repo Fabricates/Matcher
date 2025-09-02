bcd78f94b45728fb1d78bfc706d435b53802bb31
Cloorc <wittcnezh@foxmail.com>
2025-09-02 05:52:08 -0400

    enhance: remove kafka dependency
    Signed-off-by: Cloorc <wittcnezh@foxmail.com>

--- event_integration_test.go
+++ event_integration_test.go
@@ -5,46 +5,6 @@ import (
 	"testing"
 )
 
-func TestInMemoryBrokerPublishVariations(t *testing.T) {
-	broker := NewInMemoryEventBroker("test-node")
-	defer broker.Close()
-
-	// Test publishing different event types to cover more branches
-	events := []*Event{
-		{Type: EventTypeRuleAdded, NodeID: "test-node"},
-		{Type: EventTypeRuleUpdated, NodeID: "test-node"},
-		{Type: EventTypeRuleDeleted, NodeID: "test-node"},
-		{Type: EventTypeDimensionAdded, NodeID: "test-node"},
-		{Type: EventTypeDimensionUpdated, NodeID: "test-node"},
-		{Type: EventTypeDimensionDeleted, NodeID: "test-node"},
-	}
-
-	// Add a subscriber channel
-	eventChan := make(chan *Event, 10)
-	broker.Subscribe(context.Background(), eventChan)
-
-	// Publish various events
-	for _, event := range events {
-		err := broker.Publish(context.Background(), event)
-		if err != nil {
-			t.Errorf("Failed to publish event %s: %v", event.Type, err)
-		}
-	}
-
-	// Consume events from channel
-	eventsReceived := 0
-	for eventsReceived < len(events) {
-		select {
-		case <-eventChan:
-			// Event received
-			eventsReceived++
-		default:
-			// No more events available
-		}
-	}
-	close(eventChan)
-}
-
 func TestMockEventSubscriberBranches(t *testing.T) {
 	subscriber := NewMockEventSubscriber()
 	defer subscriber.Close()
